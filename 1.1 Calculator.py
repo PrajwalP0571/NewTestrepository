6d9796da6406c910edac5f775c638cb8f74775f9
PrajwalP0571 <146669008+PrajwalP0571@users.noreply.github.com>
2023-11-24 23:21:02 +0530

    Add files via upload

---
+++ 1.1 Calculator.py
@@ -0,0 +1,17 @@
+#Practical no.1
+#1 . Program for simple calculator by taking input from user:
+#   Code:
+
+print("Name: Prajwal \nRoll no: 174")
+a = int(input("enter the number 1 : "))
+b = int(input("enter the number 2 : "))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a%b
+print("The addition :",c)
+print("The subtraction :",d)
+print("The multiplication :",e)
+print("The division :",f)
+print("The module :",g)
